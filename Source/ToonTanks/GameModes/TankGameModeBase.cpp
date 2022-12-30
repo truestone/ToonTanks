ce9e14229e4d1f683edb89dcbc4104466169641e
IMAC-PC\trues <truestone@gmail.com>
2022-12-30 18:50:53 +0900

    16절 게임 모드 클래스 생성 - 완료

---
+++ Source/ToonTanks/GameModes/TankGameModeBase.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankGameModeBase.h"
+
+void ATankGameModeBase::HandleGameStart()
+{
+}
+
+void ATankGameModeBase::HandleGameOver(bool PlayerWon)
+{
+}
+
+void ATankGameModeBase::ActorDied(AActor* DeadActor)
+{
+}
+
+
+void ATankGameModeBase::BeginPlay()
+{
+}
