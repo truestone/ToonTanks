4993be2c52fef872ebde08a25723221ca3629ae3
IMAC-PC\trues <truestone@gmail.com>
2022-12-30 17:08:20 +0900

    14절 액터 스폰 - 완료

--- Source/ToonTanks/Pawns/PawnBase.cpp
+++ Source/ToonTanks/Pawns/PawnBase.cpp
@@ -3,6 +3,7 @@
 
 #include "PawnBase.h"
 #include "Components/CapsuleComponent.h"
+#include "ToonTanks/Actors/ProjectileBase.h"
 
 // Sets default values
 APawnBase::APawnBase()
@@ -39,7 +40,14 @@ void APawnBase::RotateTurret(FVector LookAtTarget)
 
 void APawnBase::Fire()
 {
-    UE_LOG(LogTemp, Warning, TEXT("Fire Condition Success"));
+    if (ProjectileClass)
+    {
+        FVector SpawnLocation = ProjectileSpawnPoint->GetComponentLocation();
+        FRotator SpawnRotation = ProjectileSpawnPoint->GetComponentRotation();
+        AProjectileBase* TempProjectile =
+            GetWorld()->SpawnActor<AProjectileBase>(ProjectileClass, SpawnLocation, SpawnRotation);
+        TempProjectile->SetOwner(this);
+    }
 }
 
 
